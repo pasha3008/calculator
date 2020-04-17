afe53bfdb2c522b39a12b003fb0420e47eab043a
kshaik <kshaik.aws30@gmail.com>
2020-04-17 20:59:20 +0530

    mod function added

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ module.exports={
     add : function(int a,int b){
 		return a+b;
     },
+	
 	divid:function(int a,int b){
 		return a/b;
+	},
+	cube:function(int a){
+		return a*a*a;
 	}
     
 };
